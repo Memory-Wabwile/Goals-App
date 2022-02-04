b78e05ee728f3188ce3e2832ede458e1210ae28d
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-04 12:58:08 +0300

    create the goals class

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -3,8 +3,3 @@ export class Goal {
   // name!: string;
   // description!: string;
 
-public showDescription: boolean;
-  constructor(public id: number,public name: string,public description: string ,public completeDate: Date ){
-    this.showDescription=false;
-  }
-}

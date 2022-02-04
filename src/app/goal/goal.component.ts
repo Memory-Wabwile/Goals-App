4bb2737d75eaeebb3bee8deee53ffcc62c115a59
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-04 14:41:49 +0300

    install bootstrap and add it onto style.css

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -4,7 +4,7 @@ import { Goal } from '../goal';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  styleUrls: ['./goal.component.css'],
 })
 export class GoalComponent implements OnInit {
   // goals: Goal[] = [
@@ -16,29 +16,57 @@ export class GoalComponent implements OnInit {
   //   {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
   // ];
 
-
   goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    new Goal(
+      1,
+      'Watch finding Nemo',
+      'Find an online version and watch merlin find his son',
+      new Date(2020, 3, 14)
+    ),
+    new Goal(
+      2,
+      'Buy Cookies',
+      'I have to buy cookies for the parrot',
+      new Date(2019, 6, 9)
+    ),
+    new Goal(
+      3,
+      'Get new Phone Case',
+      'Diana has her birthday coming up soon',
+      new Date(2022, 1, 12)
+    ),
+    new Goal(
+      4,
+      'Get Dog Food',
+      'Pupper likes expensive snacks',
+      new Date(2019, 0, 18)
+    ),
+    new Goal(5, 'Solve math homework', 'Damn Math', new Date(2019, 2, 14)),
+    new Goal(
+      6,
+      'Plot my world domination plan',
+      'Cause I am an evil overlord',
+      new Date(2030, 3, 14)
+    ),
   ];
 
-  toggleDetails(index : any){
+  toggleDetails(index: any) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
   // for when the goal is complete its triggered
-  completeGoal(isComplete:any, index:any){
+  deleteGoal(isComplete: any, index: any) {
     if (isComplete) {
-      this.goals.splice(index,1);
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.goals[index].name}?`
+      );
+
+      if (toDelete) {
+        this.goals.splice(index, 1);
+      }
     }
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

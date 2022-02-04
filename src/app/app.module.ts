a8f6de6c1bf591a2ad2877c54a4ac4433253fe38
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-04 15:10:16 +0300

    create a goal form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { GoalComponent } from './goal/goal.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DateCountPipe } from './date-count.pipe';
     GoalComponent,
     GoalDetailComponent,
     StrikethroughDirective,
-    DateCountPipe
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
